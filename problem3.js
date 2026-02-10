4e89d7f102b66a1c587e59897fbbef5de413f743
ddileepp <157563361+ddileepp@users.noreply.github.com>
2026-02-10 16:48:00 +0530

    Refactor problem3 to use filter method
    Refactor problem3 function to use filter for better readability.

--- problem3.js
+++ problem3.js
@@ -1,16 +1,7 @@
 function problem3(users) {
-  let result = [];
-
-  for (let userName in users) {
-    if (users[userName].qualification === "Masters") {
-      result.push({
-        name: userName,
-        details: users[userName]
-      });
-    }
-  }
-
-  return result;
+  return Object.keys(users).filter(function (name) {
+    return users[name].qualification === "Masters";
+  });
 }
 
 module.exports = problem3;
